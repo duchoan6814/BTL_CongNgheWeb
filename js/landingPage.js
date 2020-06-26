1aaf2a749b534c20ecee448cb77ffed6be0d8708
Đức Hoàn <hoantruong6814@gmail.com>
2020-06-26 12:06:23 +0700

    commit ngay 26/6/2020

--- js/landingPage.js
+++ js/landingPage.js
@@ -35,11 +35,122 @@ $(document).ready(function () {
   $("#dropdown-laptop").width($("#dropdown-dienThoai").width());
   $("#dropdown-dongho").width($("#dropdown-dienThoai").width());
 
-  fetch("../dataMock/dienThoai.json")
-    .then((response) => response.json())
-    .then((json) => {
-      json.map((item, index) => index < 8 ? $('.item_dien_thoai').append('<a class="col-3 " href="chiTietDienThoai.html?id='+item.id+'"><div class="item-cart"><div class="card"><img class="card-img-top" src="data:image/png;base64,'+item.hinh+'" alt=""><div class="card-body"><h4 class="card-title">'+item.tenDienThoai.split(" ").splice(2).join(" ")+'</h4><p class="card-text">'+item.gia+'</p></div></div></div></a>'): pass)
-      // $('.dien_thoai').append('<div class="col-3 item-cart"><div class="card"><img class="card-img-top" src="https://via.placeholder.com/150" alt=""><div class="card-body"><h4 class="card-title">Chao anh em</h4><p class="card-text">Text</p></div></div></div>');
+  dataDienThoai.map(
+    (item, index) =>
+      index < 8 &&
+      $(".item_dien_thoai").append(
+        '<a class="col-3 " href="chiTietDienThoai.html?id=' +
+          item.id +
+          '"><div class="item-cart"><div class="card card-dien-thoai"><img class="card-img-top" src="data:image/png;base64,' +
+          item.hinh +
+          '" alt=""><div class="card-body"><h4 class="card-title">' +
+          item.tenDienThoai.split(" ").splice(2).join(" ") +
+          '</h4><p class="card-text">' +
+          item.gia +
+          "</p></div></div></div></a>"
+      )
+  );
+  // $('.dien_thoai').append('<div class="col-3 item-cart"><div class="card"><img class="card-img-top" src="https://via.placeholder.com/150" alt=""><div class="card-body"><h4 class="card-title">Chao anh em</h4><p class="card-text">Text</p></div></div></div>');
+  dataLaptop.splice(0, 8).map((item) => {
+    $(".item-laptop").append(
+      '<a class="col-3" href=#><div class="item-cart"><div class="card card-laptop"><img class="card-img-top" src="data:image/png;base64,' +
+        item.hinh +
+        ' alt=""><div class="card-body"><h4 class="card-title">' +
+        item.tenLapTop.split(" ").slice(1).join() +
+        '</h4><p class="card-text">' +
+        item.gia +
+        "</p></div></div></div></a>"
+    );
+  });
+
+  const setMaxHeight = (str) => {
+    let maxHeight = 0;
+    $(str).each(function (indexInArray, valueOfElement) { 
+       console.log('val',$(this).height());
+       if($(this).height() > maxHeight){
+         maxHeight = $(this).height()
+       }
     });
+
+    $(str).height(maxHeight);
+  }
+
+  setMaxHeight('.card-laptop');
+  setMaxHeight('.card-dien-thoai');
+
+  const setContentOfItemDienThoai = (type) => {
+    dataIphone = dataDienThoai.filter(item => {
+      return item.hangSanXuat == type
+    })
+  
+    console.log(dataIphone);
+    
+  
+    dataIphone.map((item, index) => {
+      index < 8 &&
+      $(".item_dien_thoai").append(
+        '<a class="col-3 " href="chiTietDienThoai.html?id=' +
+          item.id +
+          '"><div class="item-cart"><div class="card card-dien-thoai"><img class="card-img-top" src="data:image/png;base64,' +
+          item.hinh +
+          '" alt=""><div class="card-body"><h4 class="card-title">' +
+          item.tenDienThoai.split(" ").splice(2).join(" ") +
+          '</h4><p class="card-text">' +
+          item.gia +
+          "</p></div></div></div></a>"
+      )
+    })
+
+    setMaxHeight('.card-dien-thoai');
+  }
+
+  $('#btnIphone').click((e) => {
+    e.preventDefault();
+    $('.dien_thoai a.nav-link').each((index, value) => {
+      console.log(value);
+      $('.dien_thoai a').removeClass('active-of-item');
+    })
+    $('#btnIphone').addClass('active-of-item');
+    $('.item_dien_thoai').empty()
+
+    setContentOfItemDienThoai('iPhone (Apple)');
+
+  })
+  $('#btnSamsung').click((e) => {
+    e.preventDefault();
+    $('.dien_thoai a.nav-link').each((index, value) => {
+      console.log(value);
+      $('.dien_thoai a').removeClass('active-of-item');
+    })
+    $('#btnSamsung').addClass('active-of-item');
+    $('.item_dien_thoai').empty()
+
+    setContentOfItemDienThoai('Samsung');
+
+  })
+  $('#btnOppo').click((e) => {
+    e.preventDefault();
+    $('.dien_thoai a.nav-link').each((index, value) => {
+      console.log(value);
+      $('.dien_thoai a').removeClass('active-of-item');
+    })
+    $('#btnOppo').addClass('active-of-item');
+    $('.item_dien_thoai').empty()
+
+    setContentOfItemDienThoai('OPPO');
+
+  })
+  $('#btnRealme').click((e) => {
+    e.preventDefault();
+    $('.dien_thoai a.nav-link').each((index, value) => {
+      console.log(value);
+      $('.dien_thoai a').removeClass('active-of-item');
+    })
+    $('#btnRealme').addClass('active-of-item');
+    $('.item_dien_thoai').empty()
+
+    setContentOfItemDienThoai('Realme');
+
+  })
 
 });
